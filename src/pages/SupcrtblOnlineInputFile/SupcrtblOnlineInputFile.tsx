d57839f58987321d749535e0fb86975eda49cc2a
ranivrsv <ranvirsv@gmail.com>
2024-03-14 01:12:24 -0400

    Updated Supcrtbl Input File

--- src/pages/SupcrtblOnlineInputFile/SupcrtblOnlineInputFile.tsx
+++ src/pages/SupcrtblOnlineInputFile/SupcrtblOnlineInputFile.tsx
@@ -1,41 +1,195 @@
 import React, { useState } from "react";
 import "../../App.scss";
-import { FormRadioVals } from "./ISubcrtbl";
 import Form from "react-bootstrap/Form";
 
-export default function SupcrtbOnlineInputFile() {
-  const [solventPhase, setSolventPhase] = useState<number>(-1);
-  const [reactionRegion, setReactionRegion] = useState<number>(-1);
-  const [plotOption, setPlotOption] = useState<number>(-1);
+interface RadioOption {
+  [header: string]: [string, Record<string, string>];
+}
 
-  const handleCheckBoxChange = (header: string, keyChange: number): void => {
+interface FormRadioOptions extends Array<RadioOption> {}
+
+export default function SupcrtbOnlineInputFile() {
+  const [firstState, setFirstState] = useState("Specify solvent phase region:");
+  const [solventPhase, setSolventPhase] = useState("");
+  const [liqVapSaturation, setLiqVapSaturation] = useState("");
+
+  const [independentVariable, setInependentVariabl] = useState("");
+  const [tabulationOptions, setTabulationOptions] = useState("");
+  const [tableIncrement, setTableIncrement] = useState("");
+  const [univariantCurve, setUnivariantCurve] = useState("");
+  const [univariantCalculation, setUnivariantCalculation] = useState("");
+
+  const [reactionRegion, setReactionRegion] = useState("");
+  const [plotOption, setPlotOption] = useState("");
+
+  const resetMap: Record<string, string[]> = {
+    "Specify solvent phase region:": [
+      "independentVariable",
+      "liqVapSaturation",
+      "tabulationOptions",
+      "tableIncrement",
+      "univariantCurve",
+      "univariantCalculation",
+    ],
+    "Specify indipendent state variable:": [
+      "tabulationOptions",
+      "tableIncrement",
+      "univariantCurve",
+      "univariantCalculation",
+    ],
+    "Specify tabulation option:": ["tableIncrement", "univariantCalculation"],
+    "Would you like to use the univariant curve option? (i.e., calculate T(logK,P) or P(logK,T):":
+      ["univariantCalculation"],
+    "Specify indipendent liq-vap saturation variable:": [],
+  };
+
+  const resetDependentStates = (currentHeader: string) => {
+    const dependencies = resetMap[currentHeader];
+    if (!dependencies) return;
+
+    dependencies.forEach((dependency) => {
+      switch (dependency) {
+        case "independentVariable":
+          setInependentVariabl("");
+          break;
+        case "liqVapSaturation":
+          setLiqVapSaturation("");
+          break;
+        case "tabulationOptions":
+          setTabulationOptions("");
+          break;
+        case "tableIncrement":
+          setTableIncrement("");
+          break;
+        case "univariantCurve":
+          setUnivariantCurve("");
+          break;
+        case "univariantCalculation":
+          setUnivariantCalculation("");
+          break;
+        // Add cases for other dependencies
+      }
+    });
+  };
+
+  const handleCheckBoxChange = (header: string, keyChange: string): void => {
     if (header === "Specify solvent phase region:") {
       setSolventPhase(keyChange);
+    } else if (header === "Specify indipendent state variable:") {
+      setInependentVariabl(keyChange);
+    } else if (header === "Specify indipendent liq-vap saturation variable:") {
+      setLiqVapSaturation(keyChange);
+    } else if (header === "Specify tabulation option:") {
+      setTabulationOptions(keyChange);
     } else if (header === "Specify Reaction region:") {
       setReactionRegion(keyChange);
     } else if (header === "Specify option for x-y plot file:") {
       setPlotOption(keyChange);
+    } else if (header === "Specify independent liq-vap saturation variable:") {
+      setLiqVapSaturation(keyChange);
+    } else if (header === "Specify table-increment option:") {
+      setTableIncrement(keyChange);
+    } else if (
+      header ===
+      "Would you like to use the univariant curve option? (i.e., calculate T(logK,P) or P(logK,T):"
+    ) {
+      setUnivariantCurve(keyChange);
+    } else if (header === "Specify univeriant calculation option:") {
+      setUnivariantCalculation(keyChange);
     }
+    resetDependentStates(header);
   };
 
-  const formRadioOptions: FormRadioVals = {
-    "Specify solvent phase region:": [
-      "One-Phase Region",
-      "Liquid Vapor Saturation Curve",
-    ],
-    "Specify indipendent state variable:": [
-      "Temprature (degCel),  density(H2O) (g/cc)",
-      "Temprature (degCel), pressure (bars)",
-    ],
-    "Specify indipendent liq-vap saturation variable:": [
-      "Temprature (degCel)",
-      "Pressure (bars)",
-    ],
-    "Specify Reaction region:": [
-      "Use existing reaction file",
-      "Build a new file",
-    ],
-  };
+  const formRadioOptions: FormRadioOptions = [
+    {
+      "Specify solvent phase region:": [
+        firstState,
+        {
+          "One-Phase Region": "Specify indipendent state variable:",
+          "Liquid Vapor Saturation Curve":
+            "Specify indipendent liq-vap saturation variable:",
+        },
+      ],
+    },
+    {
+      "Specify indipendent state variable:": [
+        solventPhase,
+        {
+          "Temprature (degCel),  density(H2O) (g/cc)":
+            "Specify tabulation option:",
+          "Temprature (degCel), pressure (bars)":
+            "Would you like to use the univariant curve option? (i.e., calculate T(logK,P) or P(logK,T):",
+        },
+      ],
+    },
+
+    {
+      "Specify indipendent liq-vap saturation variable:": [
+        solventPhase,
+        {
+          "Temprature (degCel)": "Specify table-increment option:",
+          "Pressure (bars)": "Specify table-increment option:",
+        },
+      ],
+    },
+    {
+      "Specify tabulation option:": [
+        independentVariable,
+        {
+          "Calculate ISOCHORIC (T) tables": "Specify table-increment option:",
+          "Calculate ISOTHERMAL (D) tables": "Specify table-increment option:",
+        },
+      ],
+    },
+    {
+      "Would you like to use the univariant curve option? (i.e., calculate T(logK,P) or P(logK,T):":
+        [
+          independentVariable,
+          {
+            Yes: "Specify univeriant calculation option:",
+            No: "Specify tabulation option:",
+          },
+        ],
+    },
+    {
+      "Specify tabulation option:": [
+        univariantCurve,
+        {
+          "Calculate tables having uniform increments":
+            "Specify table-increment option:",
+          "Calculate tables having unequal increments":
+            "Specify table-increment option:",
+        },
+      ],
+    },
+    {
+      "Specify univeriant calculation option:": [
+        univariantCurve,
+        {
+          "Calculate T (logK, isobars)": "T",
+          "Calculate P (logK, isotherms)": "P",
+        },
+      ],
+    },
+    {
+      "Specify table-increment option:": [
+        tabulationOptions,
+        {
+          "Calculate tables having uniform increments": "Uniform",
+          "Calculate tables having unequal increments": "Unequal",
+        },
+      ],
+    },
+    {
+      "Specify table-increment option:": [
+        liqVapSaturation,
+        {
+          "Calculate tables having uniform increments": "Uniform",
+          "Calculate tables having unequal increments": "Unequal",
+        },
+      ],
+    },
+  ];
 
   return (
     <div className="m-5 p-5">
@@ -61,34 +215,36 @@ export default function SupcrtbOnlineInputFile() {
           </Form.Select>
         </div>
       </div>
-      {Object.keys(formRadioOptions).map((elem, index) =>
-        index === 0 ||
-        index === solventPhase ||
-        index === Object.keys(formRadioOptions).length - 1 ? (
-          <Form className="my-3">
-            <b>{elem}</b>
-            <Form.Check
-              name={elem}
-              label={formRadioOptions[elem][0]}
-              value={formRadioOptions[elem][0]}
-              id={formRadioOptions[elem][0]}
-              onClick={() => handleCheckBoxChange(elem, 1)}
-              type="radio"
-            />
-            <Form.Check
-              name={elem}
-              label={formRadioOptions[elem][1]}
-              value={formRadioOptions[elem][1]}
-              id={formRadioOptions[elem][1]}
-              onClick={() => handleCheckBoxChange(elem, 2)}
-              type="radio"
-            />
+      {formRadioOptions.map((element, index) => {
+        const header = Object.keys(element)[0];
+        const [currentStateValue, optionsMap] = Object.values(element)[0];
+
+        let shouldDisplay = false;
+        if (header === currentStateValue) {
+          shouldDisplay = true;
+        }
+
+        return shouldDisplay ? (
+          <Form key={index} className="my-3">
+            <b>{header}</b>
+            {Object.entries(optionsMap).map(
+              ([optionLabel, nextState], optionIndex) => (
+                <Form.Check
+                  key={optionIndex}
+                  name={header}
+                  label={optionLabel}
+                  value={optionLabel}
+                  id={`${header}-${optionIndex}`}
+                  onClick={() => handleCheckBoxChange(header, nextState)}
+                  type="radio"
+                />
+              )
+            )}
           </Form>
-        ) : (
-          <></>
-        )
-      )}
-      {reactionRegion === 1 ? (
+        ) : null;
+      })}
+
+      {reactionRegion === "Use existing reaction file" ? (
         <div className="form-group">
           <label htmlFor="reactionFile">
             <b>Reaction File:</b>
@@ -99,7 +255,7 @@ export default function SupcrtbOnlineInputFile() {
             id="reactionFile"
           />
         </div>
-      ) : reactionRegion === 2 ? (
+      ) : reactionRegion === "Build a new file" ? (
         <div className="form-group">
           <label htmlFor="newReactionFile">
             <b>
@@ -128,7 +284,7 @@ export default function SupcrtbOnlineInputFile() {
           label="Do not generate plot files"
           value="Do not generate plot files"
           onClick={() =>
-            handleCheckBoxChange("Specify option for x-y plot file:", 1)
+            handleCheckBoxChange("Specify option for x-y plot file:", "1")
           }
           type="radio"
         />
@@ -138,7 +294,7 @@ export default function SupcrtbOnlineInputFile() {
           label="Generate plot files in generic format"
           value="Generate plot files in generic format"
           onClick={() =>
-            handleCheckBoxChange("Specify option for x-y plot file:", 2)
+            handleCheckBoxChange("Specify option for x-y plot file:", "2")
           }
           type="radio"
         />
